35501bc948ae4070ce29eb24b8c23321c588bcbd
JoyceAnelhy <santamariaanelhy21@gmail.com>
2025-06-19 13:00:04 -0500

    RENDER

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os 
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,4 +8,9 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config ['UPLOAD_FOLDER']):
+        os.makedirs(app.config [ 'UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
 
